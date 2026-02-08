820e1b652f695dae8ac8869a3c58879cf6ea1d34
Ashwini Anil  Pote <96345207+ashwinipote399@users.noreply.github.com>
2026-02-08 14:39:28 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,24 @@
+#A list is just a numbered row of boxes where you can store any information you want.
+#You use square brackets [] to make one.
+
+friends = ["john", "jimmy", "kavien","Karan", "Oscar"]
+
+# print actual list 
+print(friends)
+
+#print a particular name from list using index 
+print(friends[0])
+print(friends[2])
+
+#print the value from backward of list 
+print(friends[-1])
+print(friends[-3])
+
+#print the grabbing the elements from the list 
+print(friends[1:])
+print(friends[0:4]) #here the last item dont grab from the list
+
+#to change any item name from the list 
+friends[3] = "kavari"
+print(friends)
+
